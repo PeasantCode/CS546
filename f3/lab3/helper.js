e91756ce395c47774e813ea79ce956177b9bdee5
Jinghao Huang <huang893040504@gmail.com>
2023-07-01 22:59:20 -0400

    update users.js and create helper.js

---
+++ f3/lab3/helper.js
@@ -0,0 +1,7 @@
+export const input_check_string = (pram, pram_name) => {
+  if (!pram) throw `${pram_name} must be exist!`;
+  if (typeof pram !== "string")
+    throw `the type of ${pram_name} must be string!`;
+  pram = pram.trim();
+  if (pram.length === 0) throw `${pram_name} cannot be empty!`;
+};
